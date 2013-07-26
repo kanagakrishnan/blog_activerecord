ef12a4da35b9c6c702bf24340aade89f6e027cc4
kanaga <kanaga@kanaga.(none)>
2013-07-26 11:30:43 +0530

    now basic queries are done

--- model/query.rb
+++ model/query.rb
@@ -1,6 +1,6 @@
-require 'rubygems'
-require 'active_record'
-require 'mysql'
+"require 'rubygems'"
+require  "active_record"
+"require 'mysql'"
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 ActiveRecord::Base.establish_connection(:adapter => "mysql", :host => "localhost", :database => "blog_assignment2", :username => "root", :password => "root")
@@ -22,4 +22,26 @@ ActiveRecord::Base.establish_connection(:adapter => "mysql", :host => "localhost
 	puts "total no of blogs"
 	puts Blog.count.to_s
 
-	
+
+	User.joins(:posts).where('posts.id' => '3').each do |a|
+			puts "username for post 3 "
+			puts a.username
+		end
+
+
+	p = Post.where('blog_id' => '1')
+	puts "no of comment in blog 2"
+	puts p.size
+
+	l= Post.where(blog_id: 1).last(2)
+	l.each do |a|
+		print a.id.to_s + ":"
+		puts a.comment
+	end
+
+	f=Post.where(blog_id: 2).first(2)
+	f.each do |a|
+		print a.id.to_s + ":"
+		puts a.comment
+	end
+
